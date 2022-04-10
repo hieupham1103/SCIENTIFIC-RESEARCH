893a331cb8302176436ac14f0974713174cd27b7
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2022-04-10 09:29:53 +0700

    4-10

---
+++ CODEOVER/Function in CPP/test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int exampleFunction(int n){
+    n /= 2;
+    return n;
+}
+
+int exampleFunction(int &n){
+    n /= 2;
+}
+
+
+
+
+int main (){
+    int n = 123;
+    cout << exampleFunction(n);
+    exampleFunction
+    return 0;
+}
